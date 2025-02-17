9296bdb4e36485bb065d7d6d3628a857541c02bd
abtotss <144957937+abtotss@users.noreply.github.com>
2025-02-17 11:50:55 +0100

    Feb17 PT2

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,16 @@ Route::get('/contact', function () {
     
 });
 
+Route::get('/login', function () {
+    return view('login');
+    
+});
+
+Route::get('/register', function () {
+    return view('register');
+    
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
